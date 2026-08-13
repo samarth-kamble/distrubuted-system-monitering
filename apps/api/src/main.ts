7dc36aa22faf02f5b7e716ce63d1b35615df5bcb
Samarth Kamble <kamblesamarth04@gmail.com>
2026-08-13 10:46:04 +0530

    Scallfolding the monorepo

---
+++ apps/api/src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port =  process.env.PORT || 8080;
+
+  await app.listen(port);
+
+  console.log(`Listening on port ${port}`);
+  console.log(`App listening on port http://localhost:${port}🚀`);
+}
+
+void bootstrap();
